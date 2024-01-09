85422a23ffd4faba66a334f66c2dbf8619155b63
ali829 <alielyassraoui@gmail.com>
2024-01-09 18:50:27 +0100

    create simple calculator

---
+++ Simple-Calculator/Simple-Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2,total;
+            Console.WriteLine("write First Number :");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("write Second Number :");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            total = num1 + num2;
+            Console.WriteLine("Total :" + total);
+
+
+            Console.ReadLine();
+        }
+    }
+}
