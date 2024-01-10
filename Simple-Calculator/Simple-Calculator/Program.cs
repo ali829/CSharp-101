119d4f8b4ab63daff1c73c87396ea2337f2b44dc
ali829 <alielyassraoui@gmail.com>
2024-01-10 20:42:58 +0100

    make the calculator better :)

--- Simple-Calculator/Simple-Calculator/Program.cs
+++ Simple-Calculator/Simple-Calculator/Program.cs
@@ -11,17 +11,55 @@ namespace Simple_Calculator
         static void Main(string[] args)
         {
             double num1, num2,total;
+            string operation;
             Console.WriteLine("write First Number :");
             num1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("write Second Number :");
             num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Write the operation :");
+            operation = Console.ReadLine();
 
+            if(operation == "+")
+            {
+                total = Addition(num1, num2);
+            }
+            else if(operation == "-")
+            {
+                total = Subtraction(num1, num2);
+            }else if (operation == "*")
+            {
+                total = Multiplication(num1, num2);
+            }else if (operation == "/") {
+                total = Division(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+                Console.ReadLine();
+                return;
+            }
 
-            total = num1 + num2;
             Console.WriteLine("Total :" + total);
 
 
             Console.ReadLine();
+        }
+
+        static double Addition(double a, double b) {
+            return a + b;
+        }
+        static double Subtraction(double a , double b)
+        {
+            return a - b;
+        }
+        static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Division(double a, double b)
+        {
+            return a / b;
         }
     }
 }
