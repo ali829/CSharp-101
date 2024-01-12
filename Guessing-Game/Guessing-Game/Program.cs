69af10649c19acec010c2aa7c35f55d0572ebfaf
ali829 <alielyassraoui@gmail.com>
2024-01-12 15:18:13 +0100

    create guessing game

---
+++ Guessing-Game/Guessing-Game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "tiger";
+            string guess = "";
+            int guessLimit = 3;
+            int guessCount = 0;
+            bool outOfGuesses = false;
+
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("Guess the word :");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (!outOfGuesses)
+            {
+                Console.WriteLine("You Win !!");
+            }
+            else
+            {
+                Console.WriteLine("You Lost !!");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
