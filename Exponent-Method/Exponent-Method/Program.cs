42e55530616419f67b48e23766d20da7dac44f24
ali829 <alielyassraoui@gmail.com>
2024-01-13 12:25:26 +0100

    exponent method created

---
+++ Exponent-Method/Exponent-Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exponent_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(32, 2));
+            Console.ReadLine();
+        }
+
+
+
+        static Int64 GetPow(int powNum , int baseNum)
+        {
+            Int64 result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+
+            return result;
+        }
+    }
+}
