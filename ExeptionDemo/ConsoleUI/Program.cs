2beb2f2f56d3ea74461568beb58337efc58a451d
ali829 <alielyassraoui@gmail.com>
2024-01-15 17:19:14 +0100

    Learn exception handling :]

---
+++ ExeptionDemo/ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using ExeptionsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DemoCode demo = new DemoCode();
+            try
+            {
+                int result = demo.GrandParent(6);
+                Console.WriteLine($"The value of the given position is : {result}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+                var inner = ex.InnerException;
+
+                if(inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExeptionDemo/ExeptionsLibrary/DemoCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandParent(int position)
+        {
+            int output = 0;
+            Console.WriteLine("Open database connection!");
+            try
+            {
+                return output = Parent(position);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("You Passed In Bad Data",ex);
+            }
+            finally {
+                Console.WriteLine("Close database conection!");
+            };
+        }
+        public int Parent(int position)
+        {
+            return GetNumber(position);
+        }
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            int[] numbers = new int[] { 1, 3, 10, 89, 19 };
+
+            output = numbers[position];
+
+
+            return output;
+        }
+    }
+}
